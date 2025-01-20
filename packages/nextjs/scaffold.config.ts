48b8d51d297158889a5c1b1564b87b79a6d88847
Karateki05 <dgrua50@proton.me>
2025-01-20 23:54:55 +0000

    Update Monad Devnet URLs

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -15,16 +15,16 @@ export const monadDevnet = defineChain({
   nativeCurrency: { name: "Monad", symbol: "DMON", decimals: 18 },
   rpcUrls: {
     default: {
-      http: ["https://rpc.monad.xyz/monad"],
+      http: ["https://rpc-devnet.monadinfra.com/rpc/3fe540e310bbb6ef0b9f16cd23073b0a"],
     },
     public: {
-      http: ["https://rpc.monad.xyz/monad"],
+      http: ["https://rpc-devnet.monadinfra.com/rpc/3fe540e310bbb6ef0b9f16cd23073b0a"],
     },
   },
   blockExplorers: {
     default: {
       name: "Monad Explorer",
-      url: "https://explorer.monad.xyz",
+      url: "https://explorer.monad-devnet.devnet101.com",
     },
   },
   fees: {
